28e5adbd4bc7a79ca9546d0f7c989bd9a5cc47e7
Calcium <62979683+C-alcium@users.noreply.github.com>
2020-12-06 02:38:53 +0000

    Make some simple modifications to solve the second part of day 3

--- 3/day3.hs
+++ 3/day3.hs
@@ -4,16 +4,24 @@ tree = '#'
 main :: IO ()
 main = do
     inputs <- lines <$> readFile "input.txt"
-    let result = solve inputs
-    print result
+    let answer1 = solve inputs 1 3
+    print answer1
+    let result1 = solve inputs 1 1
+    let result2 = solve inputs 1 3
+    let result3 = solve inputs 1 5
+    let result4 = solve inputs 1 7
+    let result5 = solve inputs 2 1
+    let answer2 = result1 * result2 * result3 * result4 * result5
+    print answer2 
+    
 
 
-solve :: [String] -> Int
-solve inputs = solve 0 0 0
+solve :: [String] -> Int -> Int -> Int
+solve inputs di ri = solve 0 0 0
     where
         stepLength   = length $ head inputs
         bottomCoord  = length inputs 
         step d r     = (inputs!!d!!mod r stepLength) == tree
         solve d r c
-            | d == bottomCoord = c 
-            | otherwise        = solve (d + 1) (r + 3) (if step d r then c + 1 else c) 
+            | d >= bottomCoord = c 
+            | otherwise        = solve (d + di) (r + ri) (if step d r then c + 1 else c) 
