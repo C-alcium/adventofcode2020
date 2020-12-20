ce209426c22ee6bba1e28d2ecf75df76d9196452
Calcium <62979683+C-alcium@users.noreply.github.com>
2020-12-20 20:15:31 +0000

    Add day 10 part 1

---
+++ 10/src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "input.txt"
+  let numbers = map (\ a -> read a :: Int) input
+  let res1 = solve1 numbers 
+  print res1 
+ 
+  
+
+solve1 :: [Int] -> Int 
+solve1 xs = solve xs 0 0 0 
+  where
+    deviceJoltRating = maximum xs + 3
+    solve xs currentRating amount1 amount3 
+      | any (\ a -> (currentRating - a) == -1) xs = solve xs (currentRating + 1) (amount1 + 1) amount3
+      | any (\ a -> (currentRating - a) == -2) xs = solve xs (currentRating + 2) amount1 amount3
+      | any (\ a -> (currentRating - a) == -3) xs = solve xs (currentRating + 3) amount1 (amount3 + 1)
+      | otherwise                                 = amount1 * (amount3 + 1) 
+
